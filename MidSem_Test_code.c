16115812ecd61f31207d37b941c95792d22eb01e
Hemant Sahu <114873903+shahhemant378@users.noreply.github.com>
2025-04-10 01:14:32 +0530

    Update MidSem_Test_code.c

--- MidSem_Test_code.c
+++ MidSem_Test_code.c
@@ -17,21 +17,27 @@ void mul(int a,int b){
 }
 void div(int a,int b){
     int count=0;
-    int sum=0;
+    int remainder =0;
+     int sum=a;
     if(a<b){
         printf("The division of %d and %d is %d",a,b,0);
     }else{
-    while(sum<=a){
-        sum+=b;
+
+         while(sum>=b){
+        sum-=b;
         count++;
+            }
+             if(sum<b){
+            remainder=sum;
+              }
+              printf("The division of %d and %d is %d and remainder is %d",a,b,count,remainder);
     }
-    }
-    printf("The division of %d and %d is %d",a,b,count);
+    
 }
 void mudulo(int a ,int b){
     int sum=a;
     int i=0;
-    for(i=0;i<div(a,b);i++){
+    while(sum>=b){
         sum-=b;
     }
     printf("The mudulos of %d and %d is %d",a,b,sum);
@@ -49,22 +55,22 @@ printf("Enter two number:");
 scanf("%d%d",&a,&b);
 switch(n){
     case 1:
-    void add(a,b);
+    add(a,b);
     break;
     case 2:
-    void sub(a,b);
+   sub(a,b);
     break;
     case 3:
-    void mul(a,b);
+     mul(a,b);
     break;
     case 4:
-    void div(a,b);
+     div(a,b);
     break;
     case 5:
-    void mudulo(a,b);
+     mudulo(a,b);
     break;
     default:
     printf("You have entered wrong number or not entered and number");
 }
-    return 0;
+return 0;
 }
