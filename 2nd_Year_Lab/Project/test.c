ae5440af5bc957ad0e1ffa14d3aa16e8003912f4
Hemant Sahu <shahhemant378@gmail.com>
2025-11-02 12:54:59 +0530

    Fully Implement of code and project is not ready

--- 2nd_Year_Lab/Project/test.c
+++ 2nd_Year_Lab/Project/test.c
@@ -71,9 +71,17 @@ int* getData(int *returnSize){
     return arr;
 
 }
+
+//upgrade to the file
+//delete file
+void deleteFile(){
+    FILE *f = fopen("data.txt", "w"); //deletes the file
+}
+
 //Insertion in file
 void insertInFile(int* nums,int Size){
     FILE *f = fopen("data.txt", "a"); 
+    deleteFile();
     //add all nums data to file
     for(int i=0;i<Size;i++){
         fprintf(f,"%d,%d.\n",i,nums[i]);
@@ -82,31 +90,101 @@ void insertInFile(int* nums,int Size){
 }
 
 
-//upgrade to the file
-//delete file
-void deleteFile(){
-    FILE *f = fopen("data.txt", "w"); //deletes the file
+
+
+
+
+
+//-------------------------------------------------
+void merge(int *arr, int l, int m, int r){
+    
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
-//upgrade index
-///
 
-//main function
+// l is for left index and r is right index of the
+// sub-array of arr to be sorted
+void mergeSort(int *arr, int l, int r){
+    
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+void del(int *arr, int *size, int key) { // deleting the particular key in array
+  
+  	// Find the element
+  	int i = 0;
+  	while (arr[i] != key) i++;
+  	
+    // Shifting the right side elements one
+  	// position towards left
+    for (int j = i; j < *size - 1; j++) {
+        arr[j] = arr[j + 1];
+    }
+  
+  	// Decrease the size
+    (*size)--; 
+}
 void main(){
-//    int *nums=(int*)malloc(3*sizeof(int));
-//    nums[0]=20;
-//    nums[1]=30;
-//    nums[2]=78;
-   
-
-//     insertInFile(nums,3);
-
+// getting the data from the text file
     int size;
-
      int *arr=getData(&size);
-     for(int i=0;i<size;i++){
+    
+    mergeSort(arr, 0, size - 1); // doing sorting to the array
+    del(arr,&size,arr[2]);
+
+      for(int i=0;i<size;i++){ // print all the values of sorted array
         printf("%d ",arr[i]);
      }
-
-
-
+     arr[0]=5;  // changing the value
+    insertInFile(arr,size);// Inserting sorted array in file
 }
