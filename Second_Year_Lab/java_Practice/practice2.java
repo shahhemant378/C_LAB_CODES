63400268fb6752aa91331b3596dead3243088079
Hemant Sahu <shahhemant378@gmail.com>
2026-02-12 20:25:46 +0530

    package best example

--- Second_Year_Lab/java_Practice/practice2.java
+++ Second_Year_Lab/java_Practice/practice2.java
@@ -1,5 +1,9 @@
+
+// if both are in same pacakage then no need to import but
+// give the path from root so that it  can get 
 package C_LAB_CODES.Second_Year_Lab.java_Practice;
-import C_LAB_CODES.Second_Year_Lab.java_Practice.practice;
+//import C_LAB_CODES.Second_Year_Lab.java_Practice.practice;
+
 
 public class practice2 {
     public static void main(String[] args) {
