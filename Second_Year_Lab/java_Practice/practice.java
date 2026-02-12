68e7772731245a28c81af1ecea3f371642149c05
Hemant Sahu <shahhemant378@gmail.com>
2026-02-12 20:11:55 +0530

    practice added

--- Second_Year_Lab/java_Practice/practice.java
+++ Second_Year_Lab/java_Practice/practice.java
@@ -1,5 +1,7 @@
-package C_LAB_CODES.2nd_Year_Lab.java_Practice;
+package C_LAB_CODES.Second_Year_Lab.java_Practice;
 
 public class practice {
-    
+    public void show(){
+        System.out.println("Inside package");
+    }
 }
--- Second_Year_Lab/java_Practice/practice2.java
+++ Second_Year_Lab/java_Practice/practice2.java
@@ -1,4 +1,5 @@
-import mypack.practice;
+import C_LAB_CODES.Second_Year_Lab.java_Practice.practice;
+
 public class practice2 {
     public static void main(String[] args) {
         practice obj=new practice();
